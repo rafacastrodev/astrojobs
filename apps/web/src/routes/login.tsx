522a7e95e8554fdee19b6b39e9c8266919d41417
Rafael Castro <rafaelteicas@gmail.com>
2026-08-15 15:35:35 -0300

    first commit

---
+++ apps/web/src/routes/login.tsx
@@ -0,0 +1,9 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/login')({
+  component: LoginComponent,
+})
+
+function LoginComponent() {
+  return <div></div>
+}
