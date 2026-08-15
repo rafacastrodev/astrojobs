32d50b097a5f736f491ac64ae1188f6df4a745e8
Rafael Castro <rafaelteicas@gmail.com>
2026-08-15 15:35:35 -0300

    chore: first commit

---
+++ apps/web/src/components/Input/index.tsx
@@ -0,0 +1,5 @@
+type InputProps = React.InputHTMLAttributes<HTMLInputElement>
+
+export const Input = ({ ...props }: InputProps) => {
+  return <input {...props} />
+}
---
+++ apps/web/src/routes/__root.tsx
@@ -0,0 +1,64 @@
+import { TanStackDevtools } from '@tanstack/react-devtools'
+import type { QueryClient } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import {
+  HeadContent,
+  Scripts,
+  createRootRouteWithContext,
+} from '@tanstack/react-router'
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import appCss from '../styles.css?url'
+
+export const Route = createRootRouteWithContext<{
+  queryClient: QueryClient
+}>()({
+  head: () => ({
+    meta: [
+      {
+        charSet: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        title: 'AstroJobs',
+      },
+    ],
+    links: [
+      {
+        rel: 'stylesheet',
+        href: appCss,
+      },
+    ],
+  }),
+  shellComponent: RootDocument,
+  notFoundComponent: () => <div></div>,
+  errorComponent: () => <div></div>,
+})
+
+function RootDocument({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        {children}
+        <TanStackDevtools
+          config={{
+            position: 'bottom-right',
+          }}
+          plugins={[
+            {
+              name: 'Tanstack Router',
+              render: <TanStackRouterDevtoolsPanel />,
+            },
+          ]}
+        />
+        <Scripts />
+        <ReactQueryDevtools buttonPosition="bottom-left" />
+      </body>
+    </html>
+  )
+}
